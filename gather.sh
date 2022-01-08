c03e162f3e7ac2339de8f6e43c98080ace3d21ff
dstreufert <dan@streufert.com>
2022-01-08 02:44:14 +0000

    Added more files

---
+++ gather.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo cp /home/pi/adsbexchange/webconfig/* ./
+sudo cp /etc/sudoers.d/010_www-data ./010_www-data
+sudo cp /etc/systemd/system/webconfig.service ./
+sudo cp -r /var/www/html .
+
