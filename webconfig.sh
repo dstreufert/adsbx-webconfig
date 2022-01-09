10c4882c1c13b48887031fcf6df2f0a1ebe1d27c
dstreufert <dan@streufert.com>
2022-01-09 01:35:13 +0000

    Cleaned up webconfigure.service

--- webconfig.sh
+++ webconfig.sh
@@ -12,7 +12,7 @@ echo $USER > /tmp/webconfig/name
 chmod 777 /tmp/webconfig/*
 chmod 777 /tmp/webconfig
 
-ping 1.1.1.1 -I wlan0 -w 10 > /dev/null
+ping 1.1.1.1 -w 10 > /dev/null
 if [ $? -eq 0 ];
 then
   echo "1.1.1.1 pingable, exiting"
@@ -21,7 +21,7 @@ else
   echo "1.1.1.1 unreachable"
 fi
 
-ping 8.8.8.8 -I wlan0 -w 10 > /dev/null
+ping 8.8.8.8 -w 10 > /dev/null
 if [ $? -eq 0 ];
 then
   echo "8.8.8.8 pingable, exiting"
@@ -41,15 +41,14 @@ until [ $totalwait -gt 900 ]
 do
 	ssid=$(wpa_cli status | grep ssid | grep -v bssid | cut -d "=" -f 2)
         if [ "$ssid" = "ADSBx-config" ]; then
-		sudo ip address replace 172.23.45.1/24 dev wlan0; echo "setting wlan0 ip to 172.23.45.1/24"
+		ipset=$(ifconfig wlan0 | grep "172.23.45.1")
+		if [[ ! -z "$ipset" ]]; then
+			sudo ip address replace 172.23.45.1/24 dev wlan0; echo "setting wlan0 ip to 172.23.45.1/24"
+		fi
 		clientip=$(cat /var/lib/misc/dnsmasq.leases | head -n 1 |  cut -d " " -f3)
-		#clientip=$(arp -n | grep wlan0 | cut -d " " -f1 )
+		
                 if [[ ! -z "$clientip" ]]; then
 					echo "Client lease detected at $clientip"
-                    #ip address show wlan0 | grep 169.254.10.90 > /dev/null
-					#ipset=$?
-					#if [ $ipset -ne 0 ]; then sudo ip address replace 169.254.10.90/16 dev wlan0; echo "setting wlan0 ip to 169.254.10.90/16"; fi
-					
 				fi
         fi
 
@@ -60,9 +59,9 @@ done
 if [ "$ssid" = "ADSBx-config" ]; then
         sudo ping $clientip -I wlan0 -f -w 1; hostup=$?
                 if [ $hostup -eq 0 ]; then
-			echo "timeout tripped but client connected, disabling ADSBx-config in 900 sec"
-                        sleep 900
-			wpa_cli disable $netnum
+					echo "timeout tripped but client connected, disabling ADSBx-config in 900 sec"
+                    sleep 900
+					wpa_cli disable $netnum
                 fi
 fi
 
