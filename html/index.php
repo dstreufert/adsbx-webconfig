df6ca48e98e48c6db11353c9b3e01c76cbd553de
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2022-01-18 18:06:13 -0700

    Update index.php

--- html/index.php
+++ html/index.php
@@ -17,40 +17,38 @@ td, th {
 }  
 tr:nth-child(even) {  
   background-color: #D5D8DC;  
-}  
+}
+.btn-margin-bottom{
+    margin-bottom: 5px !important;
+}
+
 </style>
 
-
- 
 </head>
 <body>
-<div class="container">
-  <center>
-   <div class="row row-cols-1">
-     <div class="col"><h2>ADSBexchange.com<br>Custom Receiver Software<br />Main Menu</h2>
-    <br />
-      <?php
+<div class="container-fluid">
+<div class="d-grid gap-1 col-2 mx-auto">
+        <h5>ADSBexchange.com<br />Custom Receiver Software<br /><br />Main Menu</h5>
+        <?php
         $receivername=file_get_contents("/tmp/webconfig/name");
         $location = file_get_contents("/tmp/webconfig/location");
-        echo "Receiver Name:<center><pre>$receivername</pre></center>";
-        echo "Approximate Configured Location:<center><pre>$location</pre></center>";
-      ?>
-    </div>
-    <div class="d-grid gap-2 col-6 mx-auto">
-       <div class="col"><a class="btn btn-outline-primary" href="/wifi"/>Configure WiFi<a/></div>
-       <div class="col"><a class="btn btn-outline-primary" href="/config"/>Configure Receiver/Location<a/></div>
-       <div class="col"><a class="btn btn-outline-primary" href="/sdr-assign"/>Assign SDRs to services<div class="alert alert-info" role="alert">(for multiple SDR Installs)</div></a></div>
-       <div class="col"><a class="btn btn-outline-primary" href="/auth"/>Authentication and System Tools<a/></div>
-       <div class="col"><a class="btn btn-outline-primary" href="/info"/>System Info<a/></div>
-       <div class="col"><a class="btn btn-outline-primary" href="/tar1090"/>1090 map (default)<a/></div>
-       <div class="col"><a class="btn btn-outline-primary" href="/978"/>978 map (if enabled, US only)<a/></div>
-       <div class="col"><a class="btn btn-outline-primary" href="/tar1090/?pTracks"/>8 hours of tracks<a/></div>
-       <div class="col"><a class="btn btn-outline-primary" href="/graphs1090"/>graphs1090<a/></div>
-       <div class="col"><a class="btn btn-outline-primary" href="/" onclick="javascript:event.target.port=3000"/>Grafana Dashboard<a/></div>
-       <div class="col"><a class="btn btn-outline-primary" href="https://www.adsbexchange.com/myip/"/>ADSBexchange Status (/myip)<a/></div>
-    </div>
-  </div>
-  </center>
+        ?>
+        <div class="col">Receiver Name:<br /><div class="alert alert-primary" role="alert"><?php echo $receivername ?></div></div>
+        <div class="col">Approximate Configured Location:<br /><div class="alert alert-primary" role="alert"><?php echo $location ?></div></div>
+
+        <a class="btn btn-primary" href="/wifi"/>Configure WiFi<a/>
+        <a class="btn btn-primary" href="/config"/>Configure Receiver/Location<a/>
+        <a class="btn btn-primary" href="/sdr-assign"/>Assign SDRs to services <br />(for multiple SDR Installs)</a>
+        <a class="btn btn-primary" href="/auth"/>Authentication and System Tools<a/>
+        <a class="btn btn-primary" href="/info"/>System Info<a/>
+        <a class="btn btn-primary" href="/tar1090"/>1090 map (default)<a/>
+        <a class="btn btn-primary" href="/978"/>978 map (if enabled, US only)<a/>
+        <a class="btn btn-primary" href="/tar1090/?pTracks"/>8 hours of tracks<a/>
+        <a class="btn btn-primary" href="/graphs1090"/>graphs1090<a/>
+        <a class="btn btn-primary" href="/" onclick="javascript:event.target.port=3000"/>Grafana Dashboard<a/>
+        <a class="btn btn-primary" href="https://www.adsbexchange.com/myip/"/>ADSBexchange Status (/myip)<a/>
+
+        </div>
 </div>
 </body>
 </html>
