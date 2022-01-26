3d50b05aefa781bff570d97a5dc5178ee21f52c4
ADSBexchange.com <34561158+adsbxchange@users.noreply.github.com>
2022-01-26 00:27:27 -0700

    Update index.php
    refactor logic completely

--- html/config/index.php
+++ html/config/index.php
@@ -1,3 +1,6 @@
+<?php
+include('comments.php');
+?>
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -84,11 +87,11 @@ function checkcoords() {
 
 </script>
 
-<?php 
+<?php
 session_start();
 if ($_SESSION['authenticated'] != 1) {
 	$_SESSION['auth_URI'] = $_SERVER['REQUEST_URI'];
-	header("Location: ../auth"); 
+	header("Location: ../auth");
 }
 ?>
 
@@ -105,23 +108,17 @@ if ($_SESSION['authenticated'] != 1) {
 <form method='POST' name="configform" action="./index.php" onsubmit="return confirm(checkcoords() + '\nSave configuration and reboot?');">
 
 
- <?php 
-
- if (!empty($_POST["DUMP1090"])) {
-	$content=file_get_contents("/home/pi/adsbexchange/webconfig/adsb-config.txt.webtemplate");
-
-	foreach ( $_POST as $key => $value ) {
-		//echo $key . ': ' . $value;
-		//echo '<br>';
-		//echo "%%" . $key . "%%";
-		//echo '<br>';
-		$content_chunks=explode("%%" . $key . "%%", $content);
-		$content=implode($value, $content_chunks);
-
+ 	<?php
+	if (!empty($_POST["DUMP1090"])) {
+	 $new_config = '';
+	 foreach ( $_POST as $key => $value ) {
+		if($key == "USER"){
+			$new_config .= $key ."=\"".$value . "\"\n";
+		} else {
+			$new_config .= $key ."=".$value . "\n";
+		}
 	}
-
-	file_put_contents("/tmp/webconfig/adsb-config.txt", $content);
-
+	file_put_contents("/tmp/webconfig/adsb-config.txt", $new_config);
 	?>
 
 	<script type="text/javascript">
@@ -148,87 +145,176 @@ if ($_SESSION['authenticated'] != 1) {
 	echo '<p>Rebooting... visit <a href="../index.php">this link</a> to verify changes in about 70 secs..</form></body></html>';
 	system('sudo /home/pi/adsbexchange/webconfig/install-adsbconfig.sh > /dev/null 2>&1 &');
 	exit;
-}
+} // end if $_post
 
-//echo $content;
-
-$lines = file('/boot/adsb-config.txt');
 ?>
 <div class="container-sm adsbx-width">
 <table class="table table-striped table-dark">
 <?php
-foreach($lines as $line) {
-	$pos = strpos($line, "=");
-	if (substr($line, 0, 1) == '#') { $pos = false; } # If line is a comment, don't consider it a parameter even if an equal sign comes later.
-	if ($pos === false) {
-		$result .= $line;
-		if (substr($line, 1, 1) == '#') { # if 2nd character is also #, this will be a continuation of comments to be displayed with field.
-			$prevline = $prevline . substr($line, 2);
-		} else {
-			$prevline = substr($line, 2);
-		}
-	} else {
-			echo '<tr><td>';
-			$key = 	substr($line, 0, $pos);
-			$value = substr($line, $pos + 1);
-			$value = trim(str_replace('"', "", $value));
-
-			echo $prevline;
-			echo '<br>';
-			if (strtolower($value) == 'yes') {
-				?><select class="form-control" name="<?php echo $key; ?>">
-					<option value="yes" selected>yes</option>
-					<option value="no">no</option>
-				</select><?php
-
-			} elseif (strtolower($value) == 'no') {
-				?><select class="form-control" name="<?php echo $key; ?>">
-					<option value="yes">yes</option>
-					<option value="no" selected>no</option>
-				</select><?php
-
-			} elseif ($key == "GAIN") {
-				?><select class="form-control" name="<?php echo $key; ?>"><?php
+	echo '<tr><td>';
+	$lines = file('/boot/adsb-config.txt',FILE_SKIP_EMPTY_LINES);
+	//print_r($lines);
+	foreach($lines as $line) {
+    		if(!(preg_match("^#\s",$line))) {
+			$key = explode("=",$line);
+			if ($key[0] == "GAIN") {
+				echo str_replace('#','<br />',$gain."<br /><br />");
+				?><select class="form-control" name="<?php echo $key[0]; ?>"><?php
 				$gainoptions = array(-10, 0.0, 0.9, 1.4, 2.7, 3.7, 7.7, 8.7, 12.5, 14.4, 15.7, 16.6, 19.7, 20.7, 22.9, 25.4, 28.0, 29.7, 32.8, 33.8, 36.4, 37.2, 38.6, 40.2, 42.1, 43.4, 43.9, 44.5, 48.0, 49.6);
 
 				foreach ($gainoptions as $gainval) {
-					?><option value="<?php echo $gainval; ?>" <?php if ($gainval == $value) { echo 'selected'; } ?>><?php echo $gainval ; ?></option>
-					<?php
-					}
-					echo '</select>';
-
-			} elseif ($key == "LATITUDE") {
-				?>
-				<input class="form-control" type="text" name="<?php echo $key; ?>" value="<?php echo $value; ?>" pattern="[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)"/>
+				?><option value="<?php echo $gainval; ?>" <?php if ($gainval == $key[1]) { echo 'selected'; } ?>><?php echo $gainval ; ?></option>
 				<?php
-					echo '</tr></td>';
-
-			} elseif ($key == "LONGITUDE") {
-				?>
-				<input class="form-control" type="text" name="<?php echo $key; ?>" value="<?php echo $value; ?>" pattern="[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)"/>
-				<br />
-				<a class="btn btn-primary" href="javascript:alert(checkcoords())">Verify Coordinates</a>
-				<?php
-					echo '</tr></td>';
-			} elseif ($key == "ALTITUDE") {
-				?>
-				<input class="form-control" type="text" name="<?php echo $key; ?>" value="<?php echo $value; ?>" pattern="-*[0-9]{1,}(m|ft)"/>
-				<?php
-					echo '</tr></td>';
-			} elseif ($key == "USER") {
-				?>
-				<input class="form-control" type="text" name="<?php echo $key; ?>" value="<?php echo $value; ?>" pattern="[A-Za-z0-9._]+"/>
-				<?php
-					echo '</tr></td>';
-			} else {
-				?>
-				<input class="form-control" type="text" name="<?php echo $key; ?>" value="<?php echo $value; ?>" />
-				<?php
-					echo '</tr></td>';
+				}
+				echo '</select> </td></tr><tr><td>';
 			}
 
-	}
+			if ($key[0] == "LATITUDE") {
+				echo str_replace('#','<br />',$latitude."<br /><br />");
+				?>
+				<input class="form-control" type="text" name="<?php echo $key[0]; ?>" value="<?php echo $key[1]; ?>" pattern="[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)"/>
+				<?php
+				echo '</tr></td><tr><td>';
 
+			}
+			if ($key[0] == "LONGITUDE") {
+				echo str_replace('#','<br />',$longitude."<br /><br />");
+				?>
+				<input class="form-control" type="text" name="<?php echo $key[0]; ?>" value="<?php echo $key[1]; ?>" pattern="[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)"/>
+				<?php
+				echo '</tr></td><tr><td>';
+			}
+
+			if ($key[0] == "ALTITUDE") {
+				echo str_replace('#','<br />',$altitude."<br /><br />");
+				?>
+				<input class="form-control" type="text" name="<?php echo $key[0]; ?>" value="<?php echo $key[1]; ?>" pattern="-*[0-9]{1,}(m|ft)"/>
+				<?php
+				echo '</tr></td><tr><td>';
+			}
+
+			if ($key[0] == "USER") {
+				echo  str_replace('#','<br />',$mlat_name."<br /><br />");
+				$user = str_replace('"','',$key[1]);
+				?>
+				<input class="form-control" type="text" name="<?php echo $key[0]; ?>" value="<?php echo $user; ?>" pattern="[A-Za-z0-9._]+"/>
+				<?php
+				echo '</tr></td><tr><td>';
+			}
+
+			if ($key[0] == "DUMP1090") {
+                                echo  str_replace('#','<br />',$enable_1090."<br /><br />");
+				echo  'current - '.$key[1];
+                                ?>
+					<select class="form-control" name="<?php echo $key[0]; ?>">
+					<?php
+					if(str_replace(array("\n", "\t", "\r"), '', strtoupper($key[1])) == "YES"){
+					?>
+						<option value="yes" selected>yes</option>
+						<option value="no">no</option>
+					<?php
+					} else {
+					?>
+						<option value="yes">yes</option>
+						<option value="no" selected>no</option>
+					<?php
+					}
+					?>
+					</select>
+                                <?php
+                                echo '</tr></td><tr><td>';
+                        }
+
+			if ($key[0] == "DUMP978") {
+                                echo  str_replace('#','<br />',$enable_978."<br /><br />");
+                                ?>
+					<select class="form-control" name="<?php echo $key[0]; ?>">
+					<?php
+					if(str_replace(array("\n", "\t", "\r"), '', strtoupper($key[1])) == "YES"){
+					?>
+						<option value="yes" selected>yes</option>
+						<option value="no">no</option>
+					<?php
+					} else {
+					?>
+						<option value="yes">yes</option>
+						<option value="no" selected>no</option>
+					<?php
+					}
+					?>
+					</select>
+                                <?php
+                                echo '</tr></td><tr><td>';
+                        }
+
+			if ($key[0] == "AUTOGAIN") {
+                                echo  str_replace('#','<br />',$autogain."<br /><br />");
+                                ?>
+					<select class="form-control" name="<?php echo $key[0]; ?>">
+					<?php
+					if(str_replace(array("\n", "\t", "\r"), '', strtoupper($key[1])) == "YES"){
+					?>
+						<option value="yes" selected>yes</option>
+						<option value="no">no</option>
+					<?php
+					} else {
+					?>
+						<option value="yes">yes</option>
+						<option value="no" selected>no</option>
+					<?php
+					}
+					?>
+					</select>
+                                <?php
+                                echo '</tr></td><tr><td>';
+                        }
+
+			if ($key[0] == "ZEROTIER") {
+                                echo  str_replace('#','<br />',$zerotier."<br /><br />");
+                                ?>
+					<select class="form-control" name="<?php echo $key[0]; ?>">
+					<?php
+					if(str_replace(array("\n", "\t", "\r"), '', strtoupper($key[1])) == "YES"){
+					?>
+						<option value="yes" selected>yes</option>
+						<option value="no">no</option>
+					<?php
+					} else {
+					?>
+						<option value="yes">yes</option>
+						<option value="no" selected>no</option>
+					<?php
+					}
+					?>
+					</select>
+                                <?php
+                                echo '</tr></td><tr><td>';
+                        }
+
+			if ($key[0] == "PROMG") {
+                                echo  str_replace('#','<br />',$grafana."<br /><br />");
+                                ?>
+					<select class="form-control" name="<?php echo $key[0]; ?>">
+
+					<?php
+					if(str_replace(array("\n", "\t", "\r"), '', strtoupper($key[1])) == "YES"){
+					?>
+						<option value="yes" selected>yes</option>
+						<option value="no">no</option>
+					<?php
+					} else {
+					?>
+						<option value="yes">yes</option>
+						<option value="no" selected>no</option>
+					<?php
+					}
+					?>
+					</select>
+                                <?php
+                                echo '</tr></td><tr><td>';
+                        }
+
+	}
 }
 ?>
 
