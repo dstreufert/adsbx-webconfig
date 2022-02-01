6da24e2e96655e4cb3acf70411a3a25c662c4db9
Matthias Wirth <matthias.wirth@gmail.com>
2022-02-01 20:31:04 +0100

    indent webconfig.sh

--- webconfig.sh
+++ webconfig.sh
@@ -30,8 +30,8 @@ sleep 5
 iw wlan0 scan | grep SSID: | sort | uniq | cut -c 8- | grep '\S' | grep -v '\x00' > /tmp/webconfig/wifi_scan
 if [ $? -ne 0 ]
 then
-  sleep 3
-  iw wlan0 scan | grep SSID: | sort | uniq | cut -c 8- | grep '\S' | grep -v '\x00' > /tmp/webconfig/wifi_scan
+    sleep 3
+    iw wlan0 scan | grep SSID: | sort | uniq | cut -c 8- | grep '\S' | grep -v '\x00' > /tmp/webconfig/wifi_scan
 fi
 
 if [[ -z $location_not_set ]] ; then
@@ -45,19 +45,19 @@ fi
 ping 1.1.1.1 -w 10 > /dev/null
 if [ $? -eq 0 ];
 then
-  echo "1.1.1.1 pingable, exiting"
-  exit 0
+    echo "1.1.1.1 pingable, exiting"
+    exit 0
 else
-  echo "1.1.1.1 unreachable"
+    echo "1.1.1.1 unreachable"
 fi
 
 ping 8.8.8.8 -w 10 > /dev/null
 if [ $? -eq 0 ];
 then
-  echo "8.8.8.8 pingable, exiting"
-  exit 0
+    echo "8.8.8.8 pingable, exiting"
+    exit 0
 else
-  echo "8.8.8.8 unreachable"
+    echo "8.8.8.8 unreachable"
 fi
 
 
@@ -70,31 +70,31 @@ touch /tmp/webconfig_priv/unlock
 
 until [ $totalwait -gt 900 ]
 do
-	ssid=$(wpa_cli status | grep ssid | grep -v bssid | cut -d "=" -f 2)
-        if [ "$ssid" = "ADSBx-config" ]; then
-		ipset=$(ip address show dev wlan0 | grep "172.23.45.1")
-		
-			if [ -z "$ipset" ]; then
-				ip address replace 172.23.45.1/24 dev wlan0; echo "setting wlan0 ip to 172.23.45.1/24"
-			fi
-			clientip=$(cat /tmp/webconfig/dnsmasq.leases | head -n 1 |  cut -d " " -f3)
-		
-                if [[ ! -z "$clientip" ]]; then
-					echo "Client lease detected at $clientip"
-				fi
-        fi
+    ssid=$(wpa_cli status | grep ssid | grep -v bssid | cut -d "=" -f 2)
+    if [ "$ssid" = "ADSBx-config" ]; then
+        ipset=$(ip address show dev wlan0 | grep "172.23.45.1")
 
-	((totalwait++))
-        sleep 1
+        if [ -z "$ipset" ]; then
+            ip address replace 172.23.45.1/24 dev wlan0; echo "setting wlan0 ip to 172.23.45.1/24"
+        fi
+        clientip=$(cat /tmp/webconfig/dnsmasq.leases | head -n 1 |  cut -d " " -f3)
+
+        if [[ ! -z "$clientip" ]]; then
+            echo "Client lease detected at $clientip"
+        fi
+    fi
+
+    ((totalwait++))
+    sleep 1
 done
 
 if [ "$ssid" = "ADSBx-config" ]; then
-        ping $clientip -I wlan0 -f -w 1; hostup=$?
-                if [ $hostup -eq 0 ]; then
-					echo "timeout tripped but client connected, disabling ADSBx-config in 900 sec"
-                    sleep 900
-					wpa_cli disable $netnum
-                fi
+    ping $clientip -I wlan0 -f -w 1; hostup=$?
+    if [ $hostup -eq 0 ]; then
+        echo "timeout tripped but client connected, disabling ADSBx-config in 900 sec"
+        sleep 900
+        wpa_cli disable $netnum
+    fi
 fi
 
 kill $(cat /var/run/dnsmasq.pid)
