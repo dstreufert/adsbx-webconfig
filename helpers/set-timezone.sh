23ca54309dc0124a61693c675e3585c2272f092c
Matthias Wirth <matthias.wirth@gmail.com>
2022-08-28 10:49:41 +0200

    timezone stuff

--- helpers/set-timezone.sh
+++ helpers/set-timezone.sh
@@ -2,4 +2,7 @@
 
 timedatectl set-timezone "$1"
 
+# fix timedatectl doing stupid stuff (adding .. in front of /usr ???)
+dpkg-reconfigure --frontend noninteractive tzdata
+
 systemctl restart graphs1090
